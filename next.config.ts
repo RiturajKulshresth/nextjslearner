29fc6ce43697cbfe9d99fbefac668c1e25c7fc9d
RiturajKulshresth <rituraj.kulshresth@wbd.com>
2024-12-01 01:22:46 +0530

    First commit

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: [], // Add external domains if needed
+  },
+};
+
+export default nextConfig;
